289e5097ef678f309d3caa1a6f4b2bba61f0dfb8
chastine1 <batamurizashastine@gmail.com>
2018-12-17 18:33:00 +0200

    save the change

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ function leTriangle() {
     var sideone = document.getElementById("side1").value;
     var sidetwo = document.getElementById("side2").value;
     var sidethree = document.getElementById("side3").value;
+    var triangles=" Equilateral", "isosceles", "scalene";
+    console.log('this Triangle is' + triangle +"!")
     
     if (sideone===sidetwo && sideone===sidethree) {
      alert("This is an equilateral Triangle");
@@ -11,18 +13,21 @@ function leTriangle() {
      alert("This is an isosceles Triangle");
     }
     
-    else if ( sideone +sidetwo >sidethree || sidetwo +sidethree >=sideone || sideone +sidethree <= sidetwo ) {
+    else if ( sideone +sidetwo >sidethree || sidetwo +sidethree >sideone || sideone +sidethree>sidetwo ) {
      alert("This is an scalene Triangle");
     }
     
-    else 
+        
+     else (sideone +sidetwo >sidethree || sidetwo +sidethree >sideone || sideone +sidethree<sidetwo )
 
     {
-     alert("Not a Triangle! Please try again.");
+     alert("This can make triangle.");
     }
 
     if(sideOne === '' || sideTwo === '' || sideThree === '') {
       alert("Please enter three side lengths");
     }
+
+    
 
 }
