b31bb1bd67df200c6579a68fd751d7b439e232f7
chastine1 <batamurizashastine@gmail.com>
2018-12-17 11:58:59 +0200

    save the alert

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function leTriangle() {
-    var side1 = parseInt(prompt("Enter the side of SIDE1"));
-    var side2 = parseInt(prompt("Enter the side of SIDE2"));
-    var side3 = parseInt(prompt("Enter the side of SIDE3"));
+    var sideone = document.getElementById("side1").value;
+    var sidetwo = document.getElementById("side2").value;
+    var sidethree = document.getElementById("side3").value;
     
-    if (side1=== side2&& side2 === side3) {
+    if (sideone===sidetwo && sideone===sidethree) {
      alert("This is an equilateral Triangle");
     }
     
-    else if (side1 === side2|| side2===side3|| side1===side3 ){
+    else if (sideone === sidetwo || sidetwo===sidethree|| sideone ===sidethree ){
      alert("This is an isosceles Triangle");
     }
     
-    else if ( side1+side2 >side3 || side2+side3 >=side1 || side1 +side3 <= side2 ) {
+    else if ( sideone +sidetwo >sidethree || sidetwo +sidethree >=sideone || sideone +sidethree <= sidetwo ) {
      alert("This is an scalene Triangle");
     }
     
