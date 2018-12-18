3d6b72ca7602559a3604a7bd8d4f6d02ea4a7e88
chastine1 <batamurizashastine@gmail.com>
2018-12-18 10:03:35 +0200

    save the codes

--- README.md
+++ README.md
@@ -17,7 +17,11 @@ how  to used visual studio code also styling.
 
 ### BDD
 
-
+| input | output      | Behavior | Description                                                                                                |
+|-------|-------------|----------|------------------------------------------------------------------------------------------------------------|
+| 3,3,3 | equilateral | 3+3+3=9  | all sides must be equal                                                                                    |
+| 2,2,5 | isosceles   | 2+2=4    | two sides must be equal                                                                                    |
+| 3,2,4 | scalene     | 3+2=5    | none of the sides are equal to other,and when you take 2 sides together must be greater then the left side |
 
 
 
@@ -32,7 +36,7 @@ Email:batamurizashastine@gmail.com
 
 ### License and Copyright information
 
-link to go on Github:
+link to go on Github:https://github.com/chastine1/script-project
 
 MIT Copyright 2018
 
--- index.js
+++ index.js
@@ -2,32 +2,27 @@ function leTriangle() {
     var sideone = document.getElementById("side1").value;
     var sidetwo = document.getElementById("side2").value;
     var sidethree = document.getElementById("side3").value;
-    var triangles=" Equilateral", "isosceles", "scalene";
-    console.log('this Triangle is' + triangle +"!")
-    
+
+    if (sideone <= 0 || sidetwo <= 0 || sidethree <= 0)  {
+        alert("Please Enter Valid Sides");
+    }
+ 
     if (sideone===sidetwo && sideone===sidethree) {
      alert("This is an equilateral Triangle");
     }
-    
+ 
     else if (sideone === sidetwo || sidetwo===sidethree|| sideone ===sidethree ){
      alert("This is an isosceles Triangle");
     }
-    
-    else if ( sideone +sidetwo >sidethree || sidetwo +sidethree >sideone || sideone +sidethree>sidetwo ) {
+ 
+    else if ( sideone +sidetwo >sidethree || sidetwo +sidethree >=sideone || sideone +sidethree >sidetwo ) {
      alert("This is an scalene Triangle");
     }
+ 
     
-        
-     else (sideone +sidetwo >sidethree || sidetwo +sidethree >sideone || sideone +sidethree<sidetwo )
-
-    {
-     alert("This can make triangle.");
-    }
-
+ 
     if(sideOne === '' || sideTwo === '' || sideThree === '') {
-      alert("Please enter three side lengths");
-    }
-
-    
-
-}
+        alert("Please enter three side lengths");
+      }
+ 
+ }
